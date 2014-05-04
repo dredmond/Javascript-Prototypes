2fbe4af473e524164994cfbc9e3026005e51790c
Donny <donny.redmond@gmail.com>
2014-05-04 16:16:17 -0500

    Added Project Files.

---
+++ UnitCommand/Controllers/UnitLogicController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace UnitCommand.Controllers
+{
+    public class UnitLogicController : ApiController
+    {
+        [HttpGet]
+        public string TestController(string testVar)
+        {
+            return "Test Successful: " + testVar;
+        }
+    }
+}
---
+++ UnitCommand/Scripts/main.js
@@ -0,0 +1,32 @@
+﻿$(function () {
+    var mainCanvas = document.getElementById('mainCanvas'),
+        ctx = mainCanvas.getContext('2d');
+
+    $(window).resize(function() {
+        $(mainCanvas).attr('height', $(window).height() + 'px').attr('width', $(window).width() + 'px');
+        update();
+    });
+
+    $(mainCanvas).attr('height', $(window).height() + 'px').attr('width', $(window).width() + 'px');
+    
+    function update() {
+        ctx.fillStyle = '000000';
+        ctx.fillRect(0, 0, mainCanvas.width, mainCanvas.height);
+
+        ctx.font = '30px Verdana';
+        ctx.fillStyle = 'white';
+        ctx.fillText('Sample Text', mainCanvas.width / 2 - (30 * 5), (mainCanvas.height / 2) - 30);
+    }
+
+    update();
+    
+    $.ajax({
+        url: '/api/UnitLogic/TestContoller',
+        method: 'GET',
+        data: { testVar: 'this is a test variable string.'},
+        success: function(data) {
+            var testDiv = $('<div>').html(data);
+            $(body).append(testDiv);
+        }
+    });
+});
