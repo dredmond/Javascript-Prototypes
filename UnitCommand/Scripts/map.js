eabf68d64fc92ec1685582165e612ca4e1d19911
Donny Redmond <dredmond@cloud816.com>
2014-05-06 23:17:17 -0500

    Added unit.js to the project and made some changes to map.js.

--- UnitCommand/Scripts/map.js
+++ UnitCommand/Scripts/map.js
@@ -1,80 +1,91 @@
-﻿function createMap(mapInitData) {
-    var totalCells = mapInitData.height * mapInitData.width,
-        mapHeight = mapInitData.height ? mapInitData.height : 10,
-        mapWidth = mapInitData.width ? mapInitData.width : 10,
-        mapName = mapInitData.name ? mapInitData.name : 'NewMap',
-        cells = mapInitData.cells ? mapInitData.cells : [];
+﻿var map = map || (function() {
+    function createMap(mapInitData) {
+        var totalCells = mapInitData.height * mapInitData.width,
+            mapHeight = mapInitData.height ? mapInitData.height : 10,
+            mapWidth = mapInitData.width ? mapInitData.width : 10,
+            mapName = mapInitData.name ? mapInitData.name : 'NewMap',
+            cells = mapInitData.cells ? mapInitData.cells : [];
 
-    function setName(newName) {
-        mapName = newName;
-    }
+        function setName(newName) {
+            mapName = newName;
+        }
 
-    function getName() {
-        return mapName;
-    }
+        function getName() {
+            return mapName;
+        }
 
-    function getMapData() {
+        function getMapData() {
+            return {
+                name: mapName,
+                height: mapHeight,
+                width: mapWidth,
+                cells: cells
+            }
+        }
+
+        function normalizeMapLocation(x, y) {
+            // Get cell location in the array.
+            var cellIndex = coordsToArrayIndex(x, y);
+            return arrayIndexToCoords(cellIndex);
+        }
+
+        function coordsToArrayIndex(x, y) {
+            // Normalize x with width.
+            x = x % mapWidth;
+
+            var cellLocation = y * mapHeight + x;
+
+            // Make sure the cell location is never negative.
+            while (cellLocation < 0) {
+                cellLocation += totalCells;
+            }
+
+            return cellLocation;
+        }
+
+        function arrayIndexToCoords(index) {
+            // Calculate the x and y location based on the cell location in the array.
+            index = index % totalCells;
+            var y = Math.floor(index / mapHeight),
+                x = index - y * mapHeight;
+
+            return {
+                x: x,
+                y: y
+            }
+        }
+
+        function getCell(cellLocation) {
+            return cells[coordsToArrayIndex(cellLocation.x, cellLocation.y)];
+        }
+
+        function setCell(cellLocation, value) {
+            cells[coordsToArrayIndex(cellLocation.x, cellLocation.y)] = value;
+        }
+
+        function getRelativeCell(cellStartLocation, xOffset, yOffset) {
+            var loc = normalizeMapLocation(cellStartLocation.x + xOffset,
+                                           cellStartLocation.y + yOffset);
+            return getCell(loc);
+        }
+
         return {
-            name: mapName,
-            height: mapHeight,
-            width: mapWidth,
-            cells: cells
+            normalizeMapLocation: normalizeMapLocation,
+            getRelativeCell: getRelativeCell,
+            getCell: getCell,
+            setCell: setCell,
+            getMapData: getMapData,
+            getName: getName,
+            setName: setName
         }
     }
 
-    function normalizeMapLocation(x, y) {
-        // Get cell location in the array.
-        var cellIndex = coordsToArrayIndex(x, y);
-        return arrayIndexToCoords(cellIndex);
-    }
+    // Map helper functions
 
-    function coordsToArrayIndex(x, y) {
-        // Normalize x with width.
-        x = x % mapWidth;
-
-        var cellLocation = y * mapHeight + x;
-
-        // Make sure the cell location is never negative.
-        while (cellLocation < 0) {
-            cellLocation += totalCells;
-        }
-
-        return cellLocation;
-    }
-
-    function arrayIndexToCoords(index) {
-        // Calculate the x and y location based on the cell location in the array.
-        index = index % totalCells;
-        var y = Math.floor(index / mapHeight),
-            x = index - y * mapHeight;
-
-        return {
-            x: x,
-            y: y
-        }
-    }
-
-    function getCell(cellLocation) {
-        return cells[coordsToArrayIndex(cellLocation.x, cellLocation.y)];
-    }
-
-    function setCell(cellLocation, value) {
-        cells[coordsToArrayIndex(cellLocation.x, cellLocation.y)] = value;
-    }
-
-    function getRelativeCell(cellStartLocation, xOffset, yOffset) {
-        var loc = normalizeMapLocation(cellStartLocation.x + xOffset,
-                                       cellStartLocation.y + yOffset);
-        return getCell(loc);
-    }
+    // Controller save, list, load, delete functions
 
     return {
-        normalizeMapLocation: normalizeMapLocation,
-        getRelativeCell: getRelativeCell,
-        getCell: getCell,
-        setCell: setCell,
-        getMapData: getMapData,
-        getName: getName,
-        setName: setName
+        createMap: createMap
     }
-}
+})();
+
