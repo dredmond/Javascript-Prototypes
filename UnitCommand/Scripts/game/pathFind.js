777e3bd54dafc5674b0731e16584ac7e0c48b1a1
Donny Redmond <dredmond@cloud816.com>
2014-05-22 20:57:35 -0500

    Added new pathfinding js file.

---
+++ UnitCommand/Scripts/game/pathFind.js
@@ -0,0 +1,22 @@
+﻿/*
+ * Fresh start on the path finding algorithm. 
+ * The last one was getting messy and confusing.
+ * Hopefully this one will be successful. =)
+ *
+ */
+var pathFind = pathFind || (function (mapData) {
+    function tileInfo() {
+        return {
+
+        };
+    }
+
+    function execute() {
+        
+    }
+
+    return {
+        tileInfo: tileInfo,
+        execute: execute
+    };
+});
