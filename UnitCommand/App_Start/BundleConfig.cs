b37877e4d3972bb3039768567282737d28b243e1
Donny Redmond <dredmond@cloud816.com>
2014-05-29 14:27:04 -0500

    Removed pathFind.js Added a bundle for ~/Scripts/game/*.js

--- UnitCommand/App_Start/BundleConfig.cs
+++ UnitCommand/App_Start/BundleConfig.cs
@@ -8,6 +8,9 @@ namespace UnitCommand
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/game").Include(
+                        "~/Scripts/game/*.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
