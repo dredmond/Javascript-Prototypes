e09d1174e7265caf8cd633e1a75015f2ec71a013
Donny <donny.redmond@gmail.com>
2014-07-05 14:55:46 -0500

    Working on the extendableObject class.

--- UnitCommand/Scripts/game/extendableObject.js
+++ UnitCommand/Scripts/game/extendableObject.js
@@ -12,32 +12,40 @@
 
         f.prototype = source;
         destination.prototype = new f();
-        destination.prototype.constructor = prototype;
+
+        destination.prototype.constructor = destination;
     }
 
-    extendable.prototype.extend = function (obj) {
-        var base = this;
-        createPrototype(base.constructor.prototype, obj);
-        obj.prototype.parent = base.constructor.prototype;
-        return obj;
-    };
-
-    extendable.prototype.parent = function () {
-
+    extendable.prototype.extend = function (source, destination) {
+        var base = source.prototype;
+        createPrototype(base, destination);
+        destination.prototype.parent = base;
     };
 
     return new extendable();
-});
+})();
 
-var x1 = extendableObject();
+function x1() { }
+x1.prototype.test = function () {
+    console.log('testing');
+};
 
-function x1Extender() { }
+var x1Instance = new x1();
+x1Instance.test();
 
-var x2 = x1.extend({
-    extendedMethod: function () {
-        console.log(this.base.name);
+function x1Extender(name, birthday) {
+    this.name = name;
+    this.birthday = birthday;
+}
 
-        return 'this is extended in x2.';
-    }
-});
+extendableObject.extend(x1, x1Extender);
+
+x1Extender.prototype.test = function () {
+    console.log(this.name + ' ' + this.birthday);
+
+    this.parent.test();
+};
+
+var x2 = new x1Extender('donny', '8/22/1984');
+x2.test();
 
