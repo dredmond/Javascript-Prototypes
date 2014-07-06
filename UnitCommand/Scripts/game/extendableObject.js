4a4a4dc4d42d7212b41e64dc0b1d341044dc97f9
Donny <donny.redmond@gmail.com>
2014-07-06 11:02:09 -0500

    Added more code for testing.

--- UnitCommand/Scripts/game/extendableObject.js
+++ UnitCommand/Scripts/game/extendableObject.js
@@ -1,18 +1,18 @@
 ﻿var extendableObject = extendableObject || (function() {
     var hasOwnProperty = Object.prototype.hasOwnProperty;
-
+    var extendableInstance = new extendable();
     function extendable() { }
 
-    extendable.prototype.hasOwnProperty = function(propertyName) {
-        return hasOwnProperty.call(this, propertyName);
+    extendable.prototype.hasOwnProperty = function(obj, propertyName) {
+        return hasOwnProperty.call(obj, propertyName);
     }
 
     function copyProperties(source, destination) {
         for (var p in source) {
-            if (!hasOwnProperty.call(source, p))
+            if (!extendableInstance.hasOwnProperty(source, p))
                 continue;
 
-            console.log('Property:' + p);
+            console.log('Property: ' + p);
         }
     }
 
@@ -33,17 +33,20 @@
         destination.prototype.parent = base;
     };
 
-    return new extendable();
+    return extendableInstance;
 })();
 
+console.log('x1');
 function x1() { }
 x1.prototype.test = function () {
+    console.log('test called in x1.');
     console.log('testing');
 };
 
 var x1Instance = new x1();
 x1Instance.test();
 
+console.log('x1Extender inherits x1');
 function x1Extender(name, birthday) {
     this.name = name;
     this.birthday = birthday;
@@ -52,6 +55,7 @@ function x1Extender(name, birthday) {
 extendableObject.extend(x1, x1Extender);
 
 x1Extender.prototype.test = function () {
+    console.log('test called in x1Extender.');
     console.log(this.name + ' ' + this.birthday);
 
     this.parent.test();
@@ -60,6 +64,8 @@ x1Extender.prototype.test = function () {
 var x2 = new x1Extender('donny', '8/22/1984');
 x2.test();
 
+
+console.log('x2Extender inherits x1Extender');
 function x2Extender(name, birthday, age) {
     this.parent.constructor(name, birthday);
     this.age = age;
@@ -67,10 +73,18 @@ function x2Extender(name, birthday, age) {
 
 extendableObject.extend(x1Extender, x2Extender);
 
-x2Extender.prototype.test = function() {
+x2Extender.prototype.test = function () {
+    console.log('test called in x2Extender.');
     console.log(this.age);
     this.parent.test();
 };
 
 var x3 = new x2Extender('donny', '8/22/1984', '29');
 x3.test();
+
+for (var p in x3) {
+    //if (!extendableObject.hasOwnProperty(x2Extender, p))
+    //    continue;
+
+    console.log('Property: ' + p);
+}
