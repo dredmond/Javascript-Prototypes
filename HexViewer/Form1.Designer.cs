27d75516deedd52f1d00cfbb08770e6f168c73c3
Donny <donny.redmond@gmail.com>
2014-07-19 13:04:28 -0500

    Added HexViewer to the project.

---
+++ HexViewer/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace HexViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(406, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(388, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "C:\\Users\\Donny\\AppData\\Roaming\\Apple Computer\\MobileSync\\Backup\\";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(451, 41);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(681, 412);
+            this.textBox2.TabIndex = 2;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.treeView1.Location = new System.Drawing.Point(12, 41);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(433, 412);
+            this.treeView1.TabIndex = 3;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(488, 13);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(343, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(837, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1144, 465);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ HexViewer/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace HexViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear();
+
+            var rootNode = new TreeNode(textBox1.Text);
+            treeView1.Nodes.Add(rootNode);
+
+            Directory.SetCurrentDirectory(textBox1.Text);
+
+            var files = Directory.GetFiles(textBox1.Text, "*", SearchOption.AllDirectories);
+
+            foreach (var file in files)
+            {
+                rootNode.Nodes.Add(file.Replace(textBox1.Text, ""));
+            }
+            treeView1.Refresh();
+        }
+
+        private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            /*
+            textBox2.Clear();
+            textBox2.AppendText("File: " + e.Node.Text + "\r\n\r\n");
+
+            if (!File.Exists(e.Node.Text))
+                return;
+
+            var data = File.ReadAllBytes(e.Node.Text);
+
+            textBox2.AppendText(Encoding.ASCII.GetString(data));
+             */
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            textBox2.Clear();
+            textBox2.AppendText("File: " + e.Node.Text + "\r\n\r\n");
+
+            if (!File.Exists(e.Node.Text))
+                return;
+
+            var data = File.ReadAllBytes(e.Node.Text);
+            textBox2.AppendText(HexToString(12, 4, data));
+            textBox2.SuspendLayout();
+            
+            textBox2.Select(0, 0);
+            textBox2.ScrollToCaret();
+            textBox2.ResumeLayout(true);
+        }
+
+        private static string HexToString(int size, int width, byte[] data)
+        {
+            var result = new StringBuilder();
+
+            for (var i = 0; i < data.Length; i += size * width)
+            {
+                result.AppendLine(WriteHexLine(i, size, width, data) + "     " + WriteAsciiLine(i, size, width, data));
+            }
+
+            return result.ToString();
+        }
+
+        private static string WriteHexLine(int offset, int size, int width, byte[] data)
+        {
+            var result = new StringBuilder();
+            var bytesWritten = 0;
+
+            for (var i = offset; i < data.Length && bytesWritten < size * width; i++, bytesWritten++)
+            {
+                if (bytesWritten % size == 0)
+                {
+                    result.Append(" ");
+                }
+
+                result.Append(String.Format("{0:x2}", data[i]));
+            }
+
+            while (bytesWritten < size * width)
+            {
+                if (bytesWritten % size == 0)
+                {
+                    result.Append(" ");
+                }
+
+                result.Append("  ");
+                bytesWritten++;
+            }
+
+            return result.ToString();
+        }
+
+        private static string WriteAsciiLine(int offset, int size, int width, byte[] data)
+        {
+            var result = new StringBuilder();
+            var bytesWritten = 0;
+
+            for (var i = offset; i < data.Length && bytesWritten < size * width; i++, bytesWritten++)
+            {
+                if (bytesWritten % size == 0)
+                {
+                    result.Append(" ");
+                }
+
+                if (data[i] > 31 && data[i] < 128)
+                    result.Append(Convert.ToChar(data[i]));
+                else
+                    result.Append(".");
+            }
+
+            return result.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var sha1 = SHA1.Create();
+            var hashBytes = sha1.ComputeHash(Encoding.ASCII.GetBytes(textBox3.Text));
+            var hashString = "";
+
+            hashString = hashBytes.Aggregate("", (s, b) => s + String.Format("{0:x2}", b));
+
+            foreach(var node in treeView1.Nodes.)
+        }
+    }
+}
