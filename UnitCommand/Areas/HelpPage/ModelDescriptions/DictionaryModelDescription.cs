d2b58b3e4de4d6af6309d3a3393285cd97210d7f
Donny <donny.redmond@gmail.com>
2014-09-26 18:37:50 -0500

    Updated all nuget packages.

---
+++ UnitCommand/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace UnitCommand.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ UnitCommand/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace UnitCommand.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
