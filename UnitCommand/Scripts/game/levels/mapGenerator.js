3a0f82b31c8f0762bbdb00906c352e1a924f1858
Donny <donny.redmond@gmail.com>
2014-12-07 23:31:31 -0600

    Map visualization working. Now need to figure out how to structure the map properly.

--- UnitCommand/Scripts/game/levels/mapGenerator.js
+++ UnitCommand/Scripts/game/levels/mapGenerator.js
@@ -36,45 +36,109 @@ var mapGenerator = (function (difficulty) {
             console.log(totalAreas);
 
             var maxAreas = Math.ceil(totalAreas / 2);
+            for (var x = 0; x < maxAreas; x++) {
+                areas[x] = [];
+                for (var y = 0; y < maxAreas; y++) {
+                    areas[x][y] = null;
+                }
+            }
+
+            console.log(areas);
 
             // Remove start and end from totalArea Count
             totalAreas -= 2;
 
-            // Create Starting Area
-            var startArea = {
-                x: this.getRandom(0, maxAreas),
-                y: this.getRandom(0, maxAreas),
-                type: areaTypes.start
-            };
+            var areaAdded = false;
 
-            // Create Ending Area
-            var endArea = {
-                x: this.getRandom(0, maxAreas),
-                y: this.getRandom(0, maxAreas),
-                type: areaTypes.exit
-            };
+            // Create Starting Area
+            var startArea = null, endArea = null;
+
+            while (!areaAdded) {
+                // Create Starting Area
+                startArea = {
+                    x: this.getRandom(0, maxAreas),
+                    y: this.getRandom(0, maxAreas),
+                    type: areaTypes.start
+                };
+
+                areaAdded = this.addArea(startArea);
+            }
+
+            areaAdded = false;
+            while (!areaAdded) {
+                // Create Ending Area
+                endArea = {
+                    x: this.getRandom(0, maxAreas),
+                    y: this.getRandom(0, maxAreas),
+                    type: areaTypes.exit
+                };
+
+                areaAdded = this.addArea(endArea);
+            }
 
             console.log(startArea, endArea);
 
             // Build other areas and attach them.
             for (var i = 0; i < totalAreas; i++) {
-                var area = {
-                    x: this.getRandom(0, maxAreas),
-                    y: this.getRandom(0, maxAreas),
-                    type: areaTypes.basic
-                };
+                var area = null;
+                areaAdded = false;
+
+                while (!areaAdded) {
+                    area = {
+                        x: this.getRandom(0, maxAreas),
+                        y: this.getRandom(0, maxAreas),
+                        type: areaTypes.basic
+                    };
+
+                    areaAdded = this.addArea(area);
+                }
 
                 console.log(area);
             }
+
+            console.log(areas);
         },
         getRandom: function(minVal, maxVal) {
             return Math.floor(Math.min(Math.random() * maxVal + minVal, maxVal));
+        },
+        areaExists: function(x, y) {
+            return areas[x][y] !== null;
+        },
+        addArea: function(area) {
+            if (this.areaExists(area.x, area.y))
+                return false;
+
+            areas[area.x][area.y] = area;
+            return true;
+        },
+        showMap: function() {
+            var map = $('#mapArea');
+            map.empty();
+
+            for (var x = 0; x < areas.length; x++) {
+                for (var y = 0; y < areas[x].length; y++) {
+                    if (!this.areaExists(x, y))
+                        continue;
+
+                    var area = $('<div>');
+                    area.addClass('map-area').css('top', y * 20).css('left', x * 20);
+
+                    switch (areas[x][y].type) {
+                        case areaTypes.start:
+                            area.css('background-color', 'green');
+                            break;
+                        case areaTypes.exit:
+                            area.css('background-color', 'red');
+                            break;
+                        default:
+                            break;
+                    }
+
+                    map.append(area);
+                }
+            }
         }
     });
 
     return mapClass.create();
 });
-
-for (var i = 0; i < 20; i++) {
-    var map = mapGenerator(5);
-}
