4240c5678ecc7584bf919c9f93aad69c971c728e
Donny Redmond <dredmond@cloud816.com>
2014-12-17 16:12:42 -0600

    - Updated jsExtender. - Cleaned up some code. - Made mapGenerator extendable. - Added private variables to the mapClass. - Started testing out some new ways to add functions using jsExtender.

--- UnitCommand/Scripts/game/levels/mapGenerator.js
+++ UnitCommand/Scripts/game/levels/mapGenerator.js
@@ -27,121 +27,146 @@ var areaClass = (function (extension) {
     return areaBaseClass.extend(extension);
 });
 
-var mapGenerator = (function (difficulty) {
-    var areas = [],
-        areaDisplaySize = 50;
-
+var mapGenerator = (function (extension) {
     var mapClass = jsExtender({
-        constructor: function () {
-            var totalAreas = this.getRandom(difficulty, difficulty * 3);
-            console.log(totalAreas);
+        constructor: function (difficulty) {
+            var areas = [],
+                areaDisplaySize = 50,
+                proto = mapClass.prototype;
 
-            var maxAreas = Math.ceil(totalAreas / 2);
-            for (var x = 0; x < maxAreas; x++) {
-                areas[x] = [];
-                for (var y = 0; y < maxAreas; y++) {
-                    areas[x][y] = null;
+            function getRandom(minVal, maxVal) {
+                return Math.floor(Math.min(Math.random() * maxVal + minVal, maxVal));
+            }
+
+            function areaExists(x, y) {
+                return areas[x][y] !== null;
+            }
+
+            function addArea(area) {
+                if (areaExists(area.x, area.y))
+                    return false;
+
+                areas[area.x][area.y] = area;
+                return true;
+            }
+
+            function generateMap(totalAreas) {
+                if (!totalAreas || totalAreas < difficulty) {
+                    totalAreas = getRandom(difficulty, difficulty * 3);
                 }
-            }
 
-            console.log(areas);
+                console.log(totalAreas);
 
-            // Remove start and end from totalArea Count
-            totalAreas -= 2;
+                var maxAreas = Math.ceil(totalAreas / 2);
+                for (var x = 0; x < maxAreas; x++) {
+                    areas[x] = [];
+                    for (var y = 0; y < maxAreas; y++) {
+                        areas[x][y] = null;
+                    }
+                }
 
-            var areaAdded = false;
+                console.log(areas);
 
-            // Create Starting Area
-            var startArea = null, endArea = null;
+                // Remove start and end from totalArea Count
+                totalAreas -= 2;
 
-            while (!areaAdded) {
+                var areaAdded = false;
+
                 // Create Starting Area
-                startArea = {
-                    x: this.getRandom(0, maxAreas),
-                    y: this.getRandom(0, maxAreas),
-                    type: areaTypes.start
-                };
-
-                areaAdded = this.addArea(startArea);
-            }
-
-            areaAdded = false;
-            while (!areaAdded) {
-                // Create Ending Area
-                endArea = {
-                    x: this.getRandom(0, maxAreas),
-                    y: this.getRandom(0, maxAreas),
-                    type: areaTypes.exit
-                };
-
-                areaAdded = this.addArea(endArea);
-            }
-
-            console.log(startArea, endArea);
-
-            // Build other areas and attach them.
-            for (var i = 0; i < totalAreas; i++) {
-                var area = null;
-                areaAdded = false;
+                var startArea = null, endArea = null;
 
                 while (!areaAdded) {
-                    area = {
-                        x: this.getRandom(0, maxAreas),
-                        y: this.getRandom(0, maxAreas),
-                        type: areaTypes.basic
+                    // Create Starting Area
+                    startArea = {
+                        x: getRandom(0, maxAreas),
+                        y: getRandom(0, maxAreas),
+                        type: areaTypes.start
                     };
 
-                    areaAdded = this.addArea(area);
+                    areaAdded = addArea(startArea);
                 }
 
-                console.log(area);
-            }
+                areaAdded = false;
+                while (!areaAdded) {
+                    // Create Ending Area
+                    endArea = {
+                        x: getRandom(0, maxAreas),
+                        y: getRandom(0, maxAreas),
+                        type: areaTypes.exit
+                    };
 
-            console.log(areas);
-        },
-        getRandom: function(minVal, maxVal) {
-            return Math.floor(Math.min(Math.random() * maxVal + minVal, maxVal));
-        },
-        areaExists: function(x, y) {
-            return areas[x][y] !== null;
-        },
-        addArea: function(area) {
-            if (this.areaExists(area.x, area.y))
-                return false;
+                    areaAdded = addArea(endArea);
+                }
 
-            areas[area.x][area.y] = area;
-            return true;
-        },
-        showMap: function() {
-            var map = $('#mapArea');
-            map.empty();
+                console.log(startArea, endArea);
 
-            for (var x = 0; x < areas.length; x++) {
-                for (var y = 0; y < areas[x].length; y++) {
-                    if (!this.areaExists(x, y))
-                        continue;
+                // Build other areas and attach them.
+                for (var i = 0; i < totalAreas; i++) {
+                    var area = null;
+                    areaAdded = false;
 
-                    var area = $('<div>');
-                    area.addClass('map-area').css('top', y * areaDisplaySize).css('left', x * areaDisplaySize);
+                    while (!areaAdded) {
+                        area = {
+                            x: getRandom(0, maxAreas),
+                            y: getRandom(0, maxAreas),
+                            type: areaTypes.basic
+                        };
 
-                    switch (areas[x][y].type) {
-                        case areaTypes.start:
-                            area.css('background-color', 'green');
-                            area.html('START');
-                            break;
-                        case areaTypes.exit:
-                            area.css('background-color', 'red');
-                            area.html('END');
-                            break;
-                        default:
-                            break;
+                        areaAdded = addArea(area);
                     }
 
-                    map.append(area);
+                    console.log(area);
+                }
+
+                console.log(areas);
+            }
+
+            function showMap() {
+                var map = $('#mapArea');
+                map.empty();
+
+                for (var x = 0; x < areas.length; x++) {
+                    for (var y = 0; y < areas[x].length; y++) {
+                        if (!areaExists(x, y))
+                            continue;
+
+                        var area = $('<div>');
+                        area.addClass('map-area').css('top', y * areaDisplaySize).css('left', x * areaDisplaySize);
+
+                        switch (areas[x][y].type) {
+                            case areaTypes.start:
+                                area.css('background-color', 'green');
+                                area.html('START');
+                                break;
+                            case areaTypes.exit:
+                                area.css('background-color', 'red');
+                                area.html('END');
+                                break;
+                            default:
+                                break;
+                        }
+
+                        map.append(area);
+                    }
                 }
             }
+
+            proto.appendFunctions({
+                test: function () {
+                    console.log('this is a test');
+                },
+                test2: function() {
+                    console.log('test2 from mapClass.');
+                }
+            });
+
+            if (!proto.generateMap)
+                proto.generateMap = generateMap;
+
+            if (!proto.showMap)
+                proto.showMap = showMap;
         }
     });
 
-    return mapClass.create();
+    return mapClass.extend(extension);
 });
